aaa72fa3836c9779ac8725b9a1aa271270116378
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2020-06-10 19:05:07 +0530

    Update additional.dart

--- SocialAPP/lib/additional.dart
+++ SocialAPP/lib/additional.dart
@@ -12,5 +12,96 @@ import 'package:flutter/services.dart';
 import './main.dart';
 
 
+void main()
+{
+  runApp(MaterialApp(
+    home: Addition(),
+  ));
+}
 
  
+class Addition extends StatefulWidget {
+  @override
+  _AdditionState createState() => _AdditionState();
+}
+
+class _AdditionState extends State<Addition> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Instruction to follow",
+         style: TextStyle(
+               color: Colors.white,
+             ),),
+        backgroundColor: Colors.orangeAccent[700],
+        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            ),
+
+            Customize('1) *Dont press the Refresh button more than 3 times'),
+            Text("    * = Note:Pressig more than 3 times the refresh button your"),
+            Text("    operating system may damage"),
+            
+            Customize('2) If you found any error after opening the app please close&open the app again '),
+            Customize('3) If you found the data is not updating after clicking refresh button several times please close&open the app again'),
+            Customize('4) when you are using the this app it should be in the recent apps in your device'),
+            Customize("5) Any complaints and improvements regarding this app please send mail to sathyabama@gmail.com"),
+            Customize('6) If you follow these steps you can use the app in sustainable way'),
+            SizedBox(height: 20,),
+            Center(
+              child: Text('Your reponse matters....',
+              style: TextStyle(
+                letterSpacing: 2.0,
+               fontWeight: FontWeight.bold,
+               fontStyle: FontStyle.italic,
+               //color: Colors.orangeAccent[800],
+              ),),
+            ),
+
+            
+        ],
+        
+        
+      ),
+      
+
+      
+    );
+    
+  }
+}
+
+class Customize extends StatelessWidget {
+  String text;
+  Customize(this.text);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child:  Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Flexible(
+              child:  Text(
+                      text,
+                       textAlign: TextAlign.left,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 10,
+                    style: TextStyle(
+                     
+                    ),
+                    ),
+      ),
+              ],
+            ),
+
+      );
+  }
+}
+
