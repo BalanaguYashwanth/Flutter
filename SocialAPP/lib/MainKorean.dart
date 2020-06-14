5e70bf2bde4abeb2f55f3f7981486e955d0d7529
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2020-06-14 19:09:12 +0530

    Update MainKorean.dart

--- SocialAPP/lib/MainKorean.dart
+++ SocialAPP/lib/MainKorean.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
-//import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter_ble_lib/flutter_ble_lib.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
+import 'package:ble/additional.dart';
 void main() {
   runApp(MyApp());
 }
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter BLE Demo',
       home: MyHomePage(title: 'Flutter BLE Demo Page'),
     );
@@ -28,6 +29,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  String r="";
+
+
   BleManager _bleManager = BleManager(); 
   bool _isScanning= false;               
   List<BleDeviceItem> deviceList = [];   
@@ -42,6 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
     init(); 
     super.initState();
 
+
     var initializationSettingsAndroid = new AndroidInitializationSettings("app_icon");
     var initializationSettingsIos = new IOSInitializationSettings();
 
@@ -50,15 +58,11 @@ class _MyHomePageState extends State<MyHomePage> {
     flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
     flutterLocalNotificationsPlugin.initialize(initializationSettings,onSelectNotification:onSelectNotification); 
 
-
-
   }
-
-
 
   void init() async {
 
-      
+     
       await _bleManager.createClient(
         restoreStateIdentifier: "example-restore-state-identifier",
         restoreStateAction: (peripherals) {
@@ -72,19 +76,33 @@ class _MyHomePageState extends State<MyHomePage> {
         //.then((_) => _waitForBluetoothPoweredOn())        
 
         scan();
+
+
+        BluetoothState currentState = await _bleManager.bluetoothState();
+        _bleManager.observeBluetoothState().listen((btState) {
+         
+          if(btState== BluetoothState.POWERED_OFF)
+          {
+            _bleManager.enableRadio();
+          }
+          });
+
   }
   
   _checkPermissions() async {
     if (Platform.isAndroid) {
-      if (await Permission.contacts.request().isGranted) {        
+      if (await Permission.contacts.request().isGranted) {    
       }
+      
       Map<Permission, PermissionStatus> statuses = await [
         Permission.location
       ].request();
+      
       print(statuses[Permission.location]);
          
     }
   }
+
 
 
   Future onSelectNotification(String payload) {
@@ -103,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
   // ON/OFF 
   void scan() async {
     limit=0;
-    if(!_isScanning) {
+    
       deviceList.clear();
       print("deviceList1");
       
@@ -119,66 +137,82 @@ class _MyHomePageState extends State<MyHomePage> {
         print("\t${scanResult.peripheral}");
         */ 
         
-        var findDevice = deviceList.any((element) {        
+        var findDevice = deviceList.any((element)    {        
                 
-          if(element.peripheral.identifier == scanResult.peripheral.identifier)
+          if(element.peripheral.identifier == scanResult.peripheral.identifier) 
           {
             
             element.peripheral = scanResult.peripheral;
             element.advertisementData = scanResult.advertisementData;            
             element.rssi = scanResult.rssi;
-            if(element.rssi <-80)
+            if(element.rssi <-85)
             {
-              if(!devices.contains(element.peripheral))
+              if(!devices.contains(element.peripheral))  
               {
-                devices.add(element.peripheral);
+                 devices.add(element.peripheral);
                 count=count+1;
                 
                 print(limit);
                 if(count<2)
                 {
                   limit=limit+1;
-                  if(limit<5)
+                  if(limit<5) 
                   {
-                    showNotification();
-
+                    Future.delayed(const Duration(seconds: 2));
+                     showNotification();
                   }
                 }  
               }
-
             }
-
             return true;            
           }        
           return false;
         });
         
+      
         if(!findDevice) {
           count=0;
           deviceList.add(BleDeviceItem(name, scanResult.rssi, scanResult.peripheral, scanResult.advertisementData));
         }
       
-        setState((){});
+        setState((){
+          r="";
+        });
       });      
       
-      setState(() { _isScanning = true; });
-    }
-    else {      
-      print("stop");
-      _bleManager.stopPeripheralScan();      
-      setState(() { _isScanning = false; });
-    }
+     // setState(() { _isScanning = true; });
+    //}
+    // else {      
+    //   print("stop");
+    //   _bleManager.stopPeripheralScan();      
+    //   setState(() { _isScanning = false; });
+    // }
   }
   
   
   list() {
-    return ListView.builder( 
+    return ListView.builder(
+      
       itemCount: deviceList.length, 
       itemBuilder: (context, index) { 
-        return ListTile( 
-          title: Text(deviceList[index].deviceName), 
+        return ListTile(
+         contentPadding: EdgeInsets.fromLTRB(0.0, 0.0, 20.0, 0.0),
+         //leading: FlatButton.icon(onPressed:(){}, icon:Icon(Icons.bubble_chart), label: Text("$r")),
+        leading:  Text("--->$r",
+        style: TextStyle(
+          fontSize: 20,
+        ),
+        ),
+
+          title: Text(deviceList[index].deviceName,
+          style: TextStyle(
+            fontSize: 18,
+          ),), 
           subtitle: Text(deviceList[index].peripheral.identifier),
-          trailing: Text("${deviceList[index].rssi}"),          
+          trailing: Text("${deviceList[index].rssi}",
+          style: TextStyle(
+            fontSize: 18,
+          ),),          
         ); 
       },
     );
@@ -188,16 +222,64 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title:Text("WalkSafe ",
+        style: TextStyle(
+               color: Colors.white,
+             ),),
+        backgroundColor: Colors.orangeAccent[700],
+        actions: <Widget>[
+           FlatButton.icon(
+             onPressed: (){
+               Navigator.push(context, MaterialPageRoute(
+                 builder:(BuildContext context)=>Addition())
+                 );
+             },
+             label: Text("Instructions to follow",
+             style: TextStyle(
+               color: Colors.white,
+               fontSize: 16
+             ),),
+             icon:Icon( Icons.info,
+             color: Colors.white,
+             ),
+             ),
+        ],
       ),
-      body:  list(),
-      
-       floatingActionButton: FloatingActionButton(
-         onPressed: scan,        
+    
+      body:  SafeArea(
+              child:Container(
+                decoration:new BoxDecoration(
+                    color: Colors.white,
+                  image:DecorationImage(
+                    image:AssetImage('assets/download.jpeg'), 
+                    fit: BoxFit.fitWidth,
+                  colorFilter: new ColorFilter.mode(Colors.white.withOpacity(0.2), BlendMode.dstATop),
+                  ),
+                ),
+                child: list(),
+              ),
+      ),
+
+
+       floatingActionButton: Container(
+        height: 50,
+        width: 100,
         
-         child: Icon(_isScanning?Icons.stop:Icons.bluetooth_searching), 
+      child:FloatingActionButton.extended(
+         backgroundColor:Colors.orangeAccent[700],
+         label: Text("Refresh"),
+         icon:Icon(Icons.refresh) ,
+        onPressed: () async{
+          setState(() {
+           r="Refreshing..";
+          });
+          await Future.delayed(Duration(seconds:3));
+           scan();
+        },
+
+        ),
         
-       ),
+    ),
     );
   }
 
@@ -214,7 +296,7 @@ class _MyHomePageState extends State<MyHomePage> {
       androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
   await flutterLocalNotificationsPlugin.show(
     0,
-    'Safety app',
+    'WalkSafe app',
     'Requesting to maintain social distance',
     platformChannelSpecifics,
     payload: 'Please maintain the social distance',
